57f82f69405d51a642a5dcd3540d8768bb25dcb6
raj gupta <kumarraj0932@gmail.com>
2026-06-11 07:01:03 +0530

    frontend added

--- packages/judge/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { judgeQueue, addJudgeJob } from "./queue";
-// We don't export worker.ts here as it's meant to be run as a standalone process
-// and we don't want the web/socket server importing vm2 or BullMQ Worker classes accidentally.
